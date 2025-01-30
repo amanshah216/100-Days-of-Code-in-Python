88062ab318bc3584d290fda7945fc496c922d3a4
Aman Shah <aman22013061@iimscollege.edu.np>
2025-01-30 08:19:35 +0545

    Treasure Island Project

--- Day 1- Band Name Generator/task.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to my python program folks!\nThis is a Band Name Generator Project!!!")
-city_name = input("What city did you grow up in?\n")
-pet_name = input("What's your pet name?\n")
-print("Your band name is: " + city_name + " " + pet_name)
